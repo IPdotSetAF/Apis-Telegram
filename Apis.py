d7af3e8e837a1183db3d7885ac384027f64cf08c
Mohammad Mahdi Nazari <60518834+IPdotSetAF@users.noreply.github.com>
2024-11-15 10:48:03 +0330

    Update Apis.py

--- Apis.py
+++ Apis.py
@@ -2,13 +2,12 @@ __CHANNEL__ = 'Telegram : @esfelurm'
 
 import requests
 from bs4 import BeautifulSoup
-from colorama import Fore,init
+from colorama import Fore, init
 from time import sleep
+
 init()
 
-
-
-print (f"""{Fore.CYAN}
+print(f"""{Fore.CYAN}
    ____     _____     _____              ________    _____   _____      
   (    )   (  __ \   (_   _)            (___  ___)  / ___/  (_   _)     
   / /\ \    ) )_) )    | |    ________      ) )    ( (__      | |       
@@ -17,17 +16,17 @@ print (f"""{Fore.CYAN}
  /  /\  \  ( (        _| |__               ( (      \ \___  __| |___) ) 
 /__(  )__\ /__\      /_____(               /__\      \____\ \________/  
                                                                         
-    {Fore.YELLOW}Tool to get api hash and api id of telegram account 
+    {Fore.YELLOW}Tool to create an app and retrieve api hash and api id of a Telegram account 
     {Fore.LIGHTGREEN_EX}Git & Telegram : @esfelurm\n
 """)
-Phone = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter your number along with the country code [Ex : +98XXXXXX]: {Fore.RED}")
+Phone = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter your number along with the country code [Ex: +98XXXXXX]: {Fore.RED}")
 with requests.Session() as req:
     phone_number = Phone
     
     login0 = req.post('https://my.telegram.org/auth/send_password', data={'phone': phone_number})
 
     if 'Sorry, too many tries. Please try again later.' in login0.text:
-        print(f'{Fore.RED}Your account has been banned!\n Please try again in 8 hours ')
+        print(f'{Fore.RED}Your account has been banned!\n Please try again in 8 hours.')
         exit()
 
     login_data = login0.json()
@@ -43,28 +42,40 @@ with requests.Session() as req:
     
     login = req.post('https://my.telegram.org/auth/login', data=login_data)
     
+    # Create an app
+    app_name = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter a name for your app: {Fore.RED}")
+    short_name = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter a short name for your app: {Fore.RED}")
+    platform = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter the platform for your app (e.g., web, android): {Fore.RED}")
+    url = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter the URL of your app: {Fore.RED}")
+    description = input(f"{Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Enter a description for your app: {Fore.RED}")
+    
+    app_creation_data = {
+        'app_title': app_name,
+        'app_shortname': short_name,
+        'app_url': url,
+        'app_platform': platform,
+        'app_desc': description
+    }
+    
+    app_creation = req.post('https://my.telegram.org/apps/create', data=app_creation_data)
+    if app_creation.status_code == 200:
+        print(f"{Fore.GREEN}App created successfully!")
+    else:
+        print(f"{Fore.RED}Failed to create the app. Status Code: {app_creation.status_code}")
+        exit()
+    
+    # Retrieve API credentials
     apps_page = req.get('https://my.telegram.org/apps')
     soup = BeautifulSoup(apps_page.text, 'html.parser')
     try:
         api_id = soup.find('label', string='App api_id:').find_next_sibling('div').select_one('span').get_text()
         api_hash = soup.find('label', string='App api_hash:').find_next_sibling('div').select_one('span').get_text()
-        key = soup.find('label', string='Public keys:').find_next_sibling('div').select_one('code').get_text()
-        Pc = soup.find('label', string='Production configuration:').find_next_sibling('div').select_one('strong').get_text()
         sleep(3)
-        print (f"""{Fore.GREEN}
-    __    ____  ____ 
-    /__\  (  _ \(_  _)
-    /(__)\  )___/ _)(_ 
-    (__)(__)(__)  (____)
-
-    {Fore.CYAN}APIs successfully received:
+        print(f"""{Fore.GREEN}
+    APIs successfully received:
 
         {Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Api ID: {Fore.YELLOW}{api_id}
         {Fore.RED}[{Fore.GREEN}+{Fore.RED}] {Fore.GREEN}Api HASH: {Fore.YELLOW}{api_hash}
-        
-        {Fore.RED}[{Fore.GREEN}~{Fore.RED}] {Fore.GREEN}Public Key: {Fore.YELLOW}{key}
-        {Fore.RED}[{Fore.GREEN}~{Fore.RED}] {Fore.GREEN}Production configuration: {Fore.YELLOW}{Pc}
-        
     """)
-    except:
-        print (f'{Fore.RED}It is not possible to get APIs for you!\nIn the next update، this bug will be fixed!')
+    except Exception as e:
+        print(f'{Fore.RED}Failed to retrieve APIs: {e}')
